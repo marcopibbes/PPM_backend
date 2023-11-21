f7774a0373d76ec47473651592b74838c4d6fec3
Marco <103935514+marcopibbes@users.noreply.github.com>
2023-11-21 12:51:23 +0100

    Last commit (?)

--- onlineMarketPlace/settings.py
+++ onlineMarketPlace/settings.py
@@ -28,9 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-1969.up.railway.app', '127.0.0.1',]
 
-CSRF_TRUSTED_ORIGINS = ['web-production-1969.up.railway.app',]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-1969.up.railway.app',]
 
-CORS_ORIGIN_WHITELIST = ['web-production-1969.up.railway.app',]
+CORS_ORIGIN_WHITELIST = ['https://web-production-1969.up.railway.app',]
 
 
 LOGIN_URL = '/login/'
